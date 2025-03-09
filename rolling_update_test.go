9b3902f77fb89e362e550f0e2a555063c06ff24a
antonkz <anton.kz.biz@gmail.com>
2025-03-10 01:29:52 +0700

    removed redundant code

--- rolling_update_test.go
+++ rolling_update_test.go
@@ -9,7 +9,6 @@ import (
 	"github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
 	"gopkg.in/yaml.v2"
-	appsv1 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/resource"
@@ -31,7 +30,6 @@ var _ = ginkgo.Describe("Rolling Update E2E test", ginkgo.Ordered, func() {
 		hpaMaxReplicas int32
 		depStartYAML   []byte
 		hpaYAML        []byte
-		newDeployment  *appsv1.Deployment
 	)
 
 	ginkgo.BeforeAll(func() {
@@ -153,53 +151,4 @@ var _ = ginkgo.Describe("Rolling Update E2E test", ginkgo.Ordered, func() {
 
 	})
 
-	ginkgo.It("should perform rolling update with updated CPU requests", func() {
-		fmt.Printf("\n=== Triggering rolling update with new CPU requests ===\n")
-
-		currentDeployment, err := clientset.AppsV1().Deployments("test-ns").Get(
-			context.TODO(),
-			"app",
-			metav1.GetOptions{},
-		)
-		gomega.Expect(err).NotTo(gomega.HaveOccurred())
-
-		// FIX 1: Remove colon to assign to shared variable
-		newDeployment = currentDeployment.DeepCopy()
-		newDeployment.Spec.Template.Spec.Containers[0].Resources.Requests[v1.ResourceCPU] = resource.MustParse("100m")
-
-		// FIX 2: Capture updated deployment from API response
-		updatedDeployment, err := clientset.AppsV1().Deployments("test-ns").Update(
-			context.TODO(),
-			newDeployment,
-			metav1.UpdateOptions{
-				FieldManager: "e2e-test",
-			},
-		)
-		gomega.Expect(err).NotTo(gomega.HaveOccurred())
-		newDeployment = updatedDeployment
-
-		fmt.Printf("\n=== Waiting for rollout to complete ===\n")
-		gomega.Eventually(func() bool {
-			deployment, err := clientset.AppsV1().Deployments("test-ns").Get(
-				context.TODO(),
-				"app",
-				metav1.GetOptions{},
-			)
-			if err != nil {
-				return false
-			}
-			return deployment.Status.UpdatedReplicas == *deployment.Spec.Replicas &&
-				deployment.Status.Replicas == *deployment.Spec.Replicas &&
-				deployment.Status.AvailableReplicas == *deployment.Spec.Replicas
-		}, 3*time.Minute, 5*time.Second).Should(gomega.BeTrue())
-
-		// FIX 3: Refresh deployment status after rollout
-		finalDeployment, err := clientset.AppsV1().Deployments("test-ns").Get(
-			context.TODO(),
-			"app",
-			metav1.GetOptions{},
-		)
-		gomega.Expect(err).NotTo(gomega.HaveOccurred())
-		newDeployment = finalDeployment
-	})
 })
